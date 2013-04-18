364c92e8c92ceaf66844d7b6d3f8f72a3a81a25b
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-04-18 12:00:24 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -9,17 +9,18 @@ except ImportError:
 
 __author__ = 'rolandh'
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(# Distribution meta-data
-    name = "freeradius_pysaml2",
-    version = "0.0.8",
-    description = "FreeRadius python modules to be used in Moonshot",
-    author = "Roland Hedberg",
-    author_email = "roland.hedberg@adm.umu.se",
-    license = "MIT License",
-    py_modules = ['freeradius_aa','radiusd', "freeradius_ecp"],
+setup(  # Distribution meta-data
+    name="freeradius_pysaml2",
+    version="0.1.0",
+    description="FreeRadius python modules to be used in Moonshot",
+    author="Roland Hedberg",
+    author_email="roland.hedberg@adm.umu.se",
+    license="MIT License",
+    py_modules=['freeradius_aa', 'radiusd', "freeradius_ecp"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -27,21 +28,19 @@ setup(# Distribution meta-data
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        ],
+    ],
     long_description=read('README'),
-    data_files=[('/usr/local/etc/moonshot', ['etc/config.py',
-                                             'etc/metadata.xml',
-                                             "etc/pysaml_config.py"]),
-                ('/usr/local/etc/moonshot/attributemaps',
-                            ['attributemaps/basic.py',
-                             'attributemaps/saml_uri.py',
-                             'attributemaps/shibboleth_uri.py']),
-                ('/usr/local/etc/moonshot/pki',
-                            ['pki/ssl.cert', 'pki/ssl.key'])],
+    data_files=[
+        ('/usr/local/etc/moonshot',
+         ['etc/config.py', 'etc/metadata.xml', "etc/pysaml_config.py"]),
+        ('/usr/local/etc/moonshot/attributemaps',
+         ['attributemaps/basic.py', 'attributemaps/saml_uri.py',
+          'attributemaps/shibboleth_uri.py']),
+        ('/usr/local/etc/moonshot/pki', ['pki/ssl.cert', 'pki/ssl.key'])],
     zip_safe=False,
-    package_data = {"etc":["*.py", "*.xml"]},
-    include_package_data = True
+    package_data={"etc": ["*.py", "*.xml"]},
+    include_package_data=True
 #    install_requires=[
 #        'pysaml2'
 #    ]
-    )
+)
